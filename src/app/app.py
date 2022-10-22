c291f580f7ff13cf1f365c48fd1fa0de0a008dbd
jhsilvaz13 <jhsilvaz@unal.edu.co>
2022-10-22 17:53:07 -0500

    deploy vercel

--- src/app/app.py
+++ src/app/app.py
@@ -11,6 +11,3 @@ app.include_router(phone)
 
 #Create tables
 models.Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-  uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
