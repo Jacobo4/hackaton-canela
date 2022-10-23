9ac8475180ad840581ecb893c15d333255d2ab76
Juan Jacobo <juizquierdob@unal.edu.co>
2022-10-23 13:31:11 -0500

    testing upload file 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import App from './App'
 import {BrowserRouter} from "react-router-dom";
 import {registerSW} from "virtual:pwa-register";
 
-registerSW();
-
-const invervalUpdate = 30;
-
-const updateSW = registerSW({
-  onRegistered(r) {
-    r && setInterval(() => {
-      r.update()
-    }, invervalUpdate)
-  }
-});
+// registerSW();
+//
+// const invervalUpdate = 30;
+//
+// const updateSW = registerSW({
+//   onRegistered(r) {
+//     r && setInterval(() => {
+//       r.update()
+//     }, invervalUpdate)
+//   }
+// });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
