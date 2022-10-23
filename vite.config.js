67ce410f9803b890e3a7dea5a203ce1ae3339c68
Juan Jacobo <juizquierdob@unal.edu.co>
2022-10-23 12:11:55 -0500

    testing pwa 2

--- vite.config.js
+++ vite.config.js
@@ -5,60 +5,6 @@ import {VitePWA} from 'vite-plugin-pwa'
 export default defineConfig({
     plugins: [
         react(),
-        VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    ,
-            // workbox: {
-            //     globPatterns: ["*/"],
-            //     runtimeCaching: [{
-            //         handler: 'NetworkOnly',
-            //         urlPattern: /^https:\/\/api-rmc\.backefra\.com\/.*/i,
-            //         // urlPattern: /^https:\/\/api-heroes\.dev\/.*/i,
-            //         // urlPattern: /\/api\/.\/.json/,
-            //         method: 'POST',
-            //         options: {
-            //             backgroundSync: {
-            //                 name: 'myQueueName',
-            //                 options: {
-            //                     maxRetentionTime: 60 * 60 * 24 * 365 // <== 365 days
-            //                 }
-            //             }
-            //         }
-            //     },
-            //         // {
-            //         //     urlPattern: /^https:\/\/api-heroes\.dev\/.*/i,
-            //         //     handler: 'StaleWhileRevalidate',
-            //         //     options: {
-            //         //         cacheName: 'api-contenido',
-            //         //         expiration: {
-            //         //             maxEntries: 1000,
-            //         //             maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-            //         //         },
-            //         //         cacheableResponse: {
-            //         //             statuses: [0, 200]
-            //         //         }
-            //         //     }
-            //         // },
-            //         {
-            //             urlPattern: /^https:\/\/info-rmc\.backefra\.com\/.*/i,
-            //             // urlPattern: /^https:\/\/enki-v1-production\.dev\/.*/i,
-            //             handler: 'StaleWhileRevalidate',
-            //             options: {
-            //                 cacheName: 'imagen-contenido',
-            //                 expiration: {
-            //                     maxEntries: 1000,
-            //                     maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-            //                 },
-            //                 cacheableResponse: {
-            //                     statuses: [0, 200]
-            //                 }
-            //             }
-            //         }
-            //     ]
-            // }
-        })
+        VitePWA({ registerType: 'autoUpdate' })
     ]
 })
