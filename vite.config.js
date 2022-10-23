e52cd6a1c9c92e3eda20c152e6b07830f1757414
Juan Jacobo <juizquierdob@unal.edu.co>
2022-10-23 12:26:13 -0500

    testing pwa 2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,73 @@ import {VitePWA} from 'vite-plugin-pwa'
 export default defineConfig({
     plugins: [
         react(),
-        VitePWA({ registerType: 'autoUpdate' })
+        VitePWA({
+            includeAssets: ['vite.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Hackaton-canela',
+                short_name: 'Canela',
+                description: '',
+                theme_color: '#3b82f6',
+                icons: [{
+                    src: 'canelaLogo.png',
+                    sizes: '145x145',
+                    type: 'image/png',
+                },
+                    {
+                       src: 'canelaLogo.png',
+                    sizes: '145x145',
+                        type: 'image/png',
+                    }
+                ]
+            },
+            // workbox: {
+            //     globPatterns: ["*/"],
+            //     runtimeCaching: [{
+            //         handler: 'NetworkOnly',
+            //         urlPattern: /^https:\/\/api-rmc\.backefra\.com\/.*/i,
+            //         // urlPattern: /^https:\/\/api-heroes\.dev\/.*/i,
+            //         // urlPattern: /\/api\/.\/.json/,
+            //         method: 'POST',
+            //         options: {
+            //             backgroundSync: {
+            //                 name: 'myQueueName',
+            //                 options: {
+            //                     maxRetentionTime: 60 * 60 * 24 * 365 // <== 365 days
+            //                 }
+            //             }
+            //         }
+            //     },
+            //         // {
+            //         //     urlPattern: /^https:\/\/api-heroes\.dev\/.*/i,
+            //         //     handler: 'StaleWhileRevalidate',
+            //         //     options: {
+            //         //         cacheName: 'api-contenido',
+            //         //         expiration: {
+            //         //             maxEntries: 1000,
+            //         //             maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+            //         //         },
+            //         //         cacheableResponse: {
+            //         //             statuses: [0, 200]
+            //         //         }
+            //         //     }
+            //         // },
+            //         {
+            //             urlPattern: /^https:\/\/info-rmc\.backefra\.com\/.*/i,
+            //             // urlPattern: /^https:\/\/enki-v1-production\.dev\/.*/i,
+            //             handler: 'StaleWhileRevalidate',
+            //             options: {
+            //                 cacheName: 'imagen-contenido',
+            //                 expiration: {
+            //                     maxEntries: 1000,
+            //                     maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+            //                 },
+            //                 cacheableResponse: {
+            //                     statuses: [0, 200]
+            //                 }
+            //             }
+            //         }
+            //     ]
+            // }
+        })
     ]
 })
