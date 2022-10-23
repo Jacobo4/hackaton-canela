2bae959b73fc5c076f0525c2d4a164284f1a5126
Juan Jacobo <juizquierdob@unal.edu.co>
2022-10-23 14:29:59 -0500

    testing upload .-.

--- src/pages/danger/danger.jsx
+++ src/pages/danger/danger.jsx
@@ -42,37 +42,37 @@ const Danger = () => {
     }, [mediaRecorderRef, webcamRef, setCapturing]);
 
     const handleDownload = useCallback(() => {
-
-         // var myHeaders = new Headers();
-            // myHeaders.append("Cookie", "csrftoken=8p90ZUdzHT8NktpOSHlmb8t0IIj4BavzfkKnGNLfF3ljBC7uk50BnoQR4dPbE6fB");
-            //
-            // var formdata = new FormData();
-            // formdata.append("file", new File([blob], "filename"), "teAmoJhon.mp4");
-            //
-            // var requestOptions = {
-            //     method: 'POST',
-            //     headers: myHeaders,
-            //     body: formdata,
-            //     redirect: 'follow'
-            // };
-            //
-            // fetch("https://app-canela.herokuapp.com/save_media/", requestOptions)
-            //     .then(response => response.text())
-            //     .then(result => console.log(result))
-            //     .catch(error => console.log('error', error));
         if (recordedChunks.length) {
             const blob = new Blob(recordedChunks, {
                 type: "video/webm"
             });
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            document.body.appendChild(a);
-            a.style = "display: none";
-            a.href = url;
-            a.download = "react-webcam-stream-capture.webm";
-            a.click();
-            window.URL.revokeObjectURL(url);
-            setRecordedChunks([]);
+            var formdata = new FormData();
+            // formdata.append("file", new File([blob], "prruebas"));
+            formdata.append("file", new File([blob], "filename"), "teAmoJhon.mp4");
+
+
+            var requestOptions = {
+                method: 'POST',
+                body: formdata,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                redirect: 'follow'
+            };
+
+            fetch("https://app-canela.herokuapp.com/save_media/", requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
+            // const url = URL.createObjectURL(blob);
+            // const a = document.createElement("a");
+            // document.body.appendChild(a);
+            // a.style = "display: none";
+            // a.href = url;
+            // a.download = "react-webcam-stream-capture.webm";
+            // a.click();
+            // window.URL.revokeObjectURL(url);
+            // setRecordedChunks([]);
         }
     }, [recordedChunks]);
 //webgook rules
@@ -86,12 +86,15 @@ const Danger = () => {
                 {/*<button className={"grid"}>Camara</button>*/}
                 {/*<button className={"grid"}>Microfono</button>*/}
                 {capturing ? (
-                    <button className={"grid bg-red-500 text-white font-bold rounded px-4 py-2 border-0 max-w-xs"} onClick={handleStopCaptureClick}>Stop Capture</button>
+                    <button className={"grid bg-red-500 text-white font-bold rounded px-4 py-2 border-0 max-w-xs"}
+                            onClick={handleStopCaptureClick}>Stop Capture</button>
                 ) : (
-                    <button className={"grid bg-green-500 text-white font-bold rounded px-4 py-2 border-0 max-w-xs"} onClick={handleStartCaptureClick}>Start Capture</button>
+                    <button className={"grid bg-green-500 text-white font-bold rounded px-4 py-2 border-0 max-w-xs"}
+                            onClick={handleStartCaptureClick}>Start Capture</button>
                 )}
                 {recordedChunks.length > 0 && (
-                    <button className={"grid bg-red-500 text-white font-bold rounded px-4 py-2 border-0 max-w-xs"} onClick={handleDownload}>Download</button>
+                    <button className={"grid bg-red-500 text-white font-bold rounded px-4 py-2 border-0 max-w-xs"}
+                            onClick={handleDownload}>Download</button>
                 )}
 
             </div>
