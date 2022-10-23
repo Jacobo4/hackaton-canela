8ab6f29ec4338cb78cf7444886b33cd470df064f
Juan Jacobo <juizquierdob@unal.edu.co>
2022-10-23 12:56:49 -0500

    testing pwa 6

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {BrowserRouter} from "react-router-dom";
+import {registerSW} from "virtual:pwa-register";
 
+registerSW();
+
+const invervalUpdate = 30;
+
+const updateSW = registerSW({
+  onRegistered(r) {
+    r && setInterval(() => {
+      r.update()
+    }, invervalUpdate)
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
